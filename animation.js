6426a3c68c8b183443e48dcf1b7ae5e1a85eaed1
aosmann <aosmannur@gmail.com>
2021-04-02 17:26:53 +0300

    sad

---
+++ animation.js
@@ -0,0 +1,19 @@
+$(window).on("load",function() {
+   $(window).scroll(function() {
+     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+     $(".fade").each(function() {
+       /* Check the location of each desired element */
+       var objectBottom = $(this).offset().top + $(this).outerHeight();
+       
+       /* If the element is completely within bounds of the window, fade it in */
+       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+         if ($(this).css("opacity")==0) {$(this).fadeTo(100,1);}
+       } else { //object goes out of view (scrolling up)
+       }
+     });
+   }).scroll(); //invoke scroll-handler on page-load
+ });
+
+
+
+ if ($(this).css("opacity")==1) {$(this).fadeTo(10,0);}
